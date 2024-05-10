b250d8dcf941a7882883c77d31231713907dfacf
HenriDevRick <122651742+HenriDevRick@users.noreply.github.com>
2024-05-10 12:48:05 -0300

    Add files via upload

---
+++ firstProgram.py
@@ -0,0 +1,9 @@
+print("Hello!")
+
+name = input("What is your name? ")
+
+city = input("Nice to meet you, " + name + "! Where were you born? ")
+
+message = "I have heard that " + city + " is a fascinating place!"
+
+print("Hello, " + name + "! " + message)
